a641cb78419d2f5dec8f741c2d5e13ba283f7530
Aaish Sindwani <aaishsindwani@gmail.com>
2017-12-10 18:21:10 +0530

    local changes

--- abhyaas/apps/faculty/static/students/myjs.js
+++
@@ -1,4 +0,0 @@
-var main_page = ["Dashboard", "Settings", "Logout"];
-
-var course_page = ["Course Notes", "Assignment", "Inbox", "Logout"];
-
--- abhyaas/apps/students/static/students/myjs.js
+++ abhyaas/apps/students/static/students/myjs.js
@@ -1,4 +1,20 @@
 var main_page = ["Dashboard", "Settings", "Logout"];
 
 var course_page = ["Course Notes", "Assignment", "Inbox", "Logout"];
+var main_id=["dashboard","inbox","setting","logout"]
 
+function bgcol(idobject) {
+    for(var item in main_id){
+    	if(idobject==item){
+    		document.getElementById(item).style.backgroundColor='#00BCD4';
+    	}
+    	else{
+    		document.getElementById(item).style.backgroundColor='#37474F';
+    	}
+    }
+}
+
+$("a").click(function()
+{
+	alert(this.id);
+});
